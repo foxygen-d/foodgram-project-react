2a89d458b67b96533a657ca79c739f8f5c2dc68f
Anastasia Domnina <foxygen.d@yandex.ru>
2022-10-04 21:20:40 +0300

    этап 2

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,9 +2,9 @@ from http import HTTPStatus
 
 from django.http import FileResponse
 from django.shortcuts import get_object_or_404
-from djoser.views import UserViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import permissions, status, viewsets, filters
+from djoser.views import UserViewSet
+from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
