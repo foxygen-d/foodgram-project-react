a6b4beeac7c3a62ebc8a9ccc8048d1ce6ce80358
Anastasia Domnina <foxygen.d@yandex.ru>
2022-10-06 21:01:29 +0300

    этап 2 - доработки

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.5.175', 'localhost', '127.0.0.1', 'backend']
 
